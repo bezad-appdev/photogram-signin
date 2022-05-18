b0e5f280e33dff795ab9a5157747d7aa0bd63690
Beza Desta <bezad@uchicago.edu>
2022-05-18 22:25:57 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,8 @@ Rails.application.routes.draw do
   # Comment routes
 
   # CREATE
-  get("/insert_comment_record", { :controller => "comments", :action => "create" })
+  post("/insert_comment_record", { :controller => "comments", :action => "create" })
+  #post("/associate_credentials", {:controller => "comments", :action => "associate })
 
   # DELETE
 
